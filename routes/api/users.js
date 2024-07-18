9f672517e1c177a56fa6ab93520a18ae4ee93e85
klaudiab <klaudiabalas1303@gmail.com>
2024-07-18 13:49:03 +0200

    Update users.js

--- routes/api/users.js
+++ routes/api/users.js
@@ -32,14 +32,18 @@ router.post("/signup", async (req, res) => {
 });
 
 router.post("/login", async (req, res) => {
+  const loginSchema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required(),
+  });
+
+  const { error } = loginSchema.validate(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+
   const { email, password } = req.body;
-  if (!email || !password) {
-    return res.status(400).send("Email and password are required");
-  }
   const user = await getUserByEmail(email);
-  if (!user) {
-    return res.status(400).send("User not found!!!");
-  }
   try {
     const token = await loginHandler(email, password);
     return res.status(200).send(token);
@@ -50,8 +54,8 @@ router.post("/login", async (req, res) => {
 
 router.get("/logout", auth, async (req, res) => {
   try {
-    const { _id } = await req.user;
-    const user = await getUserById({ _id });
+    const { _id } = req.user;
+    const user = await getUserById(_id);
     if (!user) {
       return res.status(401).send("Not authorized");
     }
@@ -63,18 +67,14 @@ router.get("/logout", auth, async (req, res) => {
   }
 });
 
-router.get("/current", auth, async (req, res, next) => {
+router.get("/current", auth, async (req, res) => {
   const { id } = req.user;
   const user = await getUserById(id);
-  if (!user) {
-    return res.status(401).json({ message: "Not authorized" });
-  } else {
-    const userData = {
-      email: user.email,
-      subscription: user.subscription,
-    };
-    res.status(200).json(userData);
-  }
+  const userData = {
+    email: user.email,
+    subscription: user.subscription,
+  };
+  res.status(200).json(userData);
 });
 
 module.exports = router;
